186008c65b809b586075c3c297f10b5cd0addde5
Amakalu Vitalis <107583430+MerVitz@users.noreply.github.com>
2023-02-24 17:01:54 +0300

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,51 @@
+let firstCard = 10
+let secondCard = 4
+
+let cards = [firstCard, secondCard]
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+
+let cardsEl = document.querySelector("#cards-el")
+let sumEl =  document.getElementById("sum-el")
+let takeMessage = document.querySelector("#message-el")
+
+function startGame() {
+    toMove()
+}
+
+
+function toMove() {
+
+    cardsEl.textContent = "Cards: "  //+ cards[0] + " and " + cards[1]  
+
+for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " + "
+}
+
+
+
+if (sum <= 20) {
+    message = ("Do you want to draw new card?")
+}
+else if(sum === 21) {
+    message = ("You've got Blackjack!")
+    hasBlackJack = true
+}
+else{
+    message = ("You're out of the game!")
+    isAlive = false
+}
+takeMessage.textContent = message 
+sumEl.textContent = "Sum: " + sum    
+                                                                                        
+}
+
+function newcard() {
+   let card = 8
+   sum +=card
+   cards.push(card)
+   console.log(cards)
+   toMove()
+}
